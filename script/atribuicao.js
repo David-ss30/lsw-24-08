65d3601861779014acca4b3519a3278b0f6cb03a
David-ss30 <david2022silvasantos@gmail.com>
2026-08-24 09:19:48 -0300

    comparações

--- script/atribuicao.js
+++ script/atribuicao.js
@@ -1,4 +1,14 @@
-let a = 10
-let b = 20
+let a = 10;
+let b = 20;
 
-console.log("a += 5 => a = a + 5", a += 5);
+
+// console.log("a += 5 => a = a + 5", a += 5);
+console.log("a =", a);
+console.log("b =", b);
+
+aux = a;
+a = b;
+b = aux;
+
+console.log("a =", a);
+console.log("b =", b);
---
+++ script/comparacao.js
@@ -0,0 +1,14 @@
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 1.1;
+
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("c = ", c);
+console.log("d = ", d);
+console.log("a ==b ", a==b);
+console.log("a === d ", a===d);
+console.log("a !== d ", a!==d);
+console.log("a > c", a > c);
+console.log("a < b", a < b);
